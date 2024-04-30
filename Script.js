1686682475fc7108c81d0a88dbf3e27fb0329c96
Mackathleen <kathleenmachado23@gmail.com>
2024-04-30 14:00:27 -0300

    Portifólio

---
+++ Script.js
@@ -0,0 +1,15 @@
+function abrirProjeto1() {
+    var projeto1 = document.getElementById("projeto1");
+    var projeto2 = document.getElementById("projeto2");
+
+    projeto1.style.display = "block";
+    projeto2.style.display = "none";
+}
+
+function abrirProjeto2() {
+    var projeto1 = document.getElementById("projeto1");
+    var projeto2 = document.getElementById("projeto2");
+
+    projeto1.style.display = "none";
+    projeto2.style.display = "block";
+}
